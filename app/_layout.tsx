743d11c87eaccd69a078c26e72e3a5c64e921b5a
ABD ELATIF <abdelatifreziga@gmail.com>
2025-05-09 11:42:12 +0100

    to local

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { NAV_THEME } from '@/theme';
+import { ThemeProvider as NavThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme, useInitialAndroidBarSync } from '@/lib/useColorScheme';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  useInitialAndroidBarSync();
+  const { colorScheme } = useColorScheme();
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -18,12 +21,12 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <NavThemeProvider value={NAV_THEME[colorScheme]}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </NavThemeProvider>
   );
 }
